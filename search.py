8638a498824ef790cf01788eb100e64b909cdbda
Nikhil Devadoss <nikdevadoss@gmail.com>
2020-06-30 14:48:14 -0700

    file io edits

--- search.py
+++ search.py
@@ -1,11 +1,17 @@
 import sys, requests, bs4, webbrowser
 
 res = requests.get('https://google.com/search?q=' + ''.join(sys.argv[1:]))
+
 #webbrowser.open('https://google.com/search?q=' + ''.join(sys.argv[1:]))
 res.raise_for_status()
-print(res.text)
 parseTags = bs4.BeautifulSoup(res.text, "html.parser")
 elements = parseTags.select('div#main > div > div > div > a')
-numLinks = min(5, len(elements))
+numLinks = min(10, len(elements))
+fd = open('data.txt', 'r+')
+
 for i in range(numLinks):
-    webbrowser.open('https://google.com' + elements[i].get('href'))
+    fd.write(str(i + 1) + ': ')
+    query = 'https://google.com' + elements[i].get('href') + '\n'
+    fd.write(query)
+    fd.write('\n')
+    #webbrowser.open('https://google.com' + elements[i].get('href'))
